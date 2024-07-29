13ee2603265e75d3ecce2119d98ff9695b3f93ab
Gabz047 <gabe1612mail@gmail.com>
2024-07-29 15:22:09 -0300

    Created utils folder and changed boxNavigationInfo to utils folder. Also fix in v-for of header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,18 +1,12 @@
 <script setup>
-import { ref } from 'vue'
-
-const boxNavigationsInfo = ref([
-  { icon: 'home', link: '', name: 'Home' },
-  { icon: 'account-group', link: '', name: 'Members' },
-  { icon: 'note-text-outline', link: '', name: 'Projects' }
-])
+import { boxNavigationsInfo } from '@/utils/headerInformation';
 </script>
 
 <template>
   <header>
     <div class="container-header">
       <nav class="navigation-header">
-        <span class="box-span" v-for="item in boxNavigationsInfo">
+        <span class="box-span" v-for="(item, index) in boxNavigationsInfo" :key="index">
           <div class="icon-span">
             <mdicon class="vicon" :name="item.icon" :height="20" :width="20" fill="black" />
           </div>
