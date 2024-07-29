2891c7ddaba7a4d82b49a4d0e6803af3ff1d1c48
tecinfoifc <luansilva250807@gmail.com>
2024-07-29 18:47:33 -0300

    FEAT: enchance Home page #9

---
+++ src/components/homecomps/InfoComp.vue
@@ -0,0 +1,19 @@
+<template>
+      <div class="card">
+        <img :src="photo" />
+        <p style="text-align: center">{{ description }}</p>
+      </div>
+</template>
+<script setup>
+defineProps({
+  id: {
+    type: Number
+  },
+  photo: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+})
+</script>
---
+++ src/utils/info.js
@@ -0,0 +1,12 @@
+export const infoshome = [
+    {
+      id: 1,
+      photo: "https://s7g10.scene7.com/is/image/stena/foto_happy_visuals_1908-1:Large",
+      description: 'conheça nossa equipe abrindo a aba ao lado e clique em "Members" '
+    },
+    {
+      id: 2,
+      photo: "https://s7g10.scene7.com/is/image/stena/foto_happy_visuals_1908-1:Large",
+      description: 'conheça alguns de nossos projetos abrindo a aba ao lado e clique em "Projects"'
+    },
+]
