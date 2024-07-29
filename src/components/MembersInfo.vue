c7d31797719aa452d78b5361936f34b92cfcad8f
lotinmaria <mariaholotin2007@gmail.com>
2024-07-29 20:40:28 -0300

    FEAT: create Membes info component #3

---
+++ src/components/MembersInfo.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    name: String,
+    image: String,
+    about: String,
+})
+</script>
+
+<template>
+    <main class='detail-Container'>
+        <div class='detail-header'>
+            <h1 class='detail-tittle'>{{ props.name }}</h1>
+        </div>
+        <div class='detail-content'>
+            <div class='detail-article'>
+                {{ props.about }}
+            </div>
+            <div class='detail-article'>
+                <div class="box-img">
+                <img :src="props.image" alt="">
+            </div>
+            </div>
+</main>
+</template>
---
+++ src/views/memberTestView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MembersInfo from '../components/MembersInfo.vue'
+</script>
+
+<template>
+  <MembersInfo
+    name="nome"
+    about="nofaisfoijgkem je oafjio as fisajfoi jasoif io safhoifshao icsx sahdoasihdoi asiohoidjoif osfiosahd iuhsaiudh as iushfsahoidfhsoa iouh"
+  />
+</template>
+
+<style></style>
