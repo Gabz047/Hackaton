a5522385e95004860f71a4fb84ff9ac2003ac94b
Gabz047 <gabe1612mail@gmail.com>
2024-07-29 12:53:23 -0300

    Created Header component style

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,32 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const boxNavigationsInfo = ref([
+  { icon: 'home', link: '', name: 'Home' },
+  { icon: 'account-group', link: '', name: 'Members' },
+  { icon: 'note-text-outline', link: '', name: 'Projects' }
+])
+</script>
+
 <template>
-    <header>
-        <div class="container-header">
-            <nav class="navigation-header">
-            <span class="box-span">
-                <div class="icon-span">
-                    <mdicon class="vicon" name="home" :height="20" :width="20" fill="black" />
-                </div>
-                <div class="text-span">
-                    <router-link>Home</router-link> 
-                </div>
-            </span>
-            <span class="box-span">
-                <div class="icon-span">
-                    <mdicon class="vicon" name="account-group" :height="20" :width="20" fill="black" />
-                </div>
-                <div class="text-span">
-                    <router-link>Members</router-link> 
-                </div>
-            </span>
-            <span class="box-span">
-                <div class="icon-span">
-                    <mdicon class="vicon" name="note-text-outline" :height="20" :width="20" fill="black" />
-                </div>
-                <div class="text-span">
-                   <router-link>Projects</router-link> 
-                </div>
-            </span>
-        </nav>
-        </div>
-    </header>
+  <header>
+    <div class="container-header">
+      <nav class="navigation-header">
+        <span class="box-span" v-for="item in boxNavigationsInfo">
+          <div class="icon-span">
+            <mdicon class="vicon" :name="item.icon" :height="20" :width="20" fill="black" />
+          </div>
+          <div class="text-span">
+            <router-link class="text-span-content"><p>
+                {{ item.name }}
+            </p></router-link>
+          </div>
+        </span>
+      </nav>
+    </div>
+  </header>
 </template>
