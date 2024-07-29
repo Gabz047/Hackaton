ce0868c6ebbc72d306ca76ed24464446651e65f6
Gabz047 <gabe1612mail@gmail.com>
2024-07-29 11:57:26 -0300

    Created Header component and added npm vue material design icons to the project

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <div class="container-header">
+            <nav class="navigation-header">
+            <span class="box-span">
+                <div class="icon-span">
+                    <mdicon class="vicon" name="home" :height="20" :width="20" fill="black" />
+                </div>
+                <div class="text-span">
+                    <router-link>Home</router-link> 
+                </div>
+            </span>
+            <span class="box-span">
+                <div class="icon-span">
+                    <mdicon class="vicon" name="account-group" :height="20" :width="20" fill="black" />
+                </div>
+                <div class="text-span">
+                    <router-link>Members</router-link> 
+                </div>
+            </span>
+            <span class="box-span">
+                <div class="icon-span">
+                    <mdicon class="vicon" name="note-text-outline" :height="20" :width="20" fill="black" />
+                </div>
+                <div class="text-span">
+                   <router-link>Projects</router-link> 
+                </div>
+            </span>
+        </nav>
+        </div>
+    </header>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(mdiVue, {
+    icons: mdijs
+})
 app.mount('#app')
