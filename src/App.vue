f2120cc89b201570cf563e0671f9f2f8b861ca8f
Gabz047 <gabe1612mail@gmail.com>
2024-07-30 07:49:28 -0300

    Deleted section identifier

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
-import SectionIdentifierComponent from './components/SectionIdentifierComponent.vue';
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 const route = new useRoute()
@@ -13,6 +12,5 @@ const showSection = computed(() => route.name)
  
   <HeaderComponent />
   <router-view/>
-  <SectionIdentifierComponent :tittle="showSection" />
   <FooterComponent/>
 </template>
