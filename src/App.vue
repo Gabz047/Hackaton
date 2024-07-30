82d4dd6130e78b41e519380aca308ee104992e99
Gabz047 <gabe1612mail@gmail.com>
2024-07-30 00:10:45 -0300

    Created SectionIdentifier component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
+import SectionIdentifierComponent from './components/SectionIdentifierComponent.vue';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const route = new useRoute()
+
+const showSection = computed(() => route.name)
 </script>
 
 <template>
+ 
   <HeaderComponent />
   <router-view/>
+  <SectionIdentifierComponent :tittle="showSection" />
   <FooterComponent/>
 </template>
